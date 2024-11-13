394b8cb09b863ba854f5ac4ec63ddfabb86b474a
SeaCannon1122 <coroc@buzzmail.eu>
2024-11-13 13:55:04 +0100

    robot calibration

--- robot_constants.py
+++ robot_constants.py
@@ -5,10 +5,10 @@ lower_leg_length = 136.0
 hip_to_shoulder = 50.0
 
 leg_channels = [
-    [15, 14, 13],
-    [12, 11, 10],
-    [9, 8, 7],
-    [6, 5, 4]
+    [0, 1, 2],
+    [4, 5, 6],
+    [8, 9, 10],
+    [12, 13, 14]
 ]
 
 legs_initial_positions = [
