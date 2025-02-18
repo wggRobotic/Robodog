68e4660d8fb044f0201e023840cd0f7048e5ff02
Friedrich Worch <f_w_wgg@e.mail.de>
2025-02-18 15:30:45 +0100

    did stupid shit ;)

--- robot_constants.py
+++ robot_constants.py
@@ -4,8 +4,8 @@ upper_leg_length = 108.5
 lower_leg_length = 136.0
 hip_to_shoulder = 50.0
 
-leg_channels = [
-    [0, 1, 2],
+leg_ids = [
+    [(0,2), 1, 2],
     [4, 5, 6],
     [8, 9, 10],
     [12, 13, 14]
@@ -17,3 +17,13 @@ legs_initial_positions = [
     [0.0, 0.0, 150.0],
     [0.0, 0.0, 150.0],
 ]
+
+BAUDRATE1                    = 1000000           # STServo default baudrate : 1000000
+BAUDRATE2                    = 1000000           # STServo default baudrate : 1000000
+DEVICENAME1                  = 'COM11'    # Check which port is being used on your controller
+DEVICENAME2                  = 'COM12'    # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
+
+STS_MINIMUM_POSITION_VALUE  = 0             # SCServo will rotate between this value
+STS_MAXIMUM_POSITION_VALUE  = 4095
+STS_MOVING_SPEED            = 2400          # SCServo moving speed
+STS_MOVING_ACC              = 50            # SCServo moving acc
--- servo_control.py
+++ servo_control.py
@@ -1,29 +1,87 @@
-#servo lines have to be uncommented on execution machine but will give errors otherwise because of obviously unavailable i2c libraries
-
 import math
-#from adafruit_servokit import ServoKit
-
-kit = None
-
-servo_rest_positions = [
-    8200, 3000, 4200, 6666,
-    2600, 6700, 5400, 6666, 
-    6300, 3600, 4600, 6666, 
-    4200, 7100, 5300, 6666,
-]
-
-servo_ranges = [
-    -3300, 3400, -3000, 3333,
-    3500, -3600, 3600, 3333, 
-    -3300, 3400, -3000, 3333, 
-    3500, -3500, 3200, 3333
-]
+from STservo_sdk import *  # Uses STServo SDK library
+import robot_constants as rc
 
 
-def servo_control_init():
-    global kit
-    #kit = ServoKit(channels = 16)
+# Initialize PortHandler instance
+# Set the port path
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+port_handler1 = PortHandler(rc.DEVICENAME1)
+port_handler2 = PortHandler(rc.DEVICENAME2)
+
+# Initialize PacketHandler instance
+# Get methods and members of Protocol
+packetHandler1 = sts(port_handler1)
+packetHandler2 = sts(port_handler2)
+
+# Open port
+if port_handler1.openPort():
+    print("Succeeded to open the port")
+else:
+    print("Failed to open the port")
+
+
+if port_handler2.openPort():
+    print("Succeeded to open the port")
+else:
+    print("Failed to open the port")
+
+
+# Set port baudrate
+if port_handler1.setBaudRate(rc.BAUDRATE1):
+    print("Succeeded to set baudrate")
+else:
+    print("Failed to set baudrate")
+
+if port_handler2.setBaudRate(rc.BAUDRATE2):
+    print("Succeeded to set baudrate")
+else:
+    print("Failed to set baudrate")
+
+
+def map_value(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def servo_move(channel: int, angle: float):
-    1==1
-    #kit.servo[channel]._pwm_out.duty_cycle = int(servo_rest_positions[channel] + servo_ranges[channel] * angle * 2 / math.pi)
+    STS_MOVING_SPEED = 2400  # Standard-Geschwindigkeit
+    STS_MOVING_ACC = 50  # Standard-Beschleunigung
+
+    # Map angle to servo position
+    servo_position = map_value(angle, -math.pi/2, math.pi/2, rc.STS_MINIMUM_POSITION_VALUE, rc.STS_MAXIMUM_POSITION_VALUE)
+
+    # Set goal position
+    sts_comm_result, sts_error = packetHandler1.write2ByteTxRx(port_handler1, channel, STS_GOAL_POSITION_L, int(servo_position))
+    if sts_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler1.getTxRxResult(sts_comm_result))
+    elif sts_error != 0:
+        print("%s" % packetHandler1.getRxPacketError(sts_error))
+
+    # Syncwrite goal position
+    sts_comm_result = packetHandler1.groupSyncWrite.txPacket()
+    if sts_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler1.getTxRxResult(sts_comm_result))
+
+    # Clear syncwrite parameter storage
+    packetHandler1.groupSyncWrite.clearParam()
+
+def servo_move(channel: int, angle: float):
+    STS_MOVING_SPEED = 2400  # Standard-Geschwindigkeit
+    STS_MOVING_ACC = 50  # Standard-Beschleunigung
+
+    # Map angle to servo position
+    servo_position = map_value(angle, -math.pi/2, math.pi/2, rc.STS_MINIMUM_POSITION_VALUE, rc.STS_MAXIMUM_POSITION_VALUE)
+
+    # Set goal position
+    sts_comm_result, sts_error = packetHandler2.write2ByteTxRx(port_handler2, channel, STS_GOAL_POSITION_L, int(servo_position))
+    if sts_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(sts_comm_result))
+    elif sts_error != 0:
+        print("%s" % packetHandler.getRxPacketError(sts_error))
+
+    # Syncwrite goal position
+    sts_comm_result = packetHandler.groupSyncWrite.txPacket()
+    if sts_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(sts_comm_result))
+
+    # Clear syncwrite parameter storage
+    packetHandler.groupSyncWrite.clearParam()
