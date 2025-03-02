80dc09dd1cdb34b77834916f82c9154919e5bdb5
Jonas Nicklas <jonasnicklas@mein.gmx>
2025-03-02 21:39:25 +0100

    maybe roll fixed?

--- idefix/robot_dog.py
+++ idefix/robot_dog.py
@@ -13,12 +13,8 @@ class RobotDog:
         self.body_length = body_length
         self.body_width = body_width
         self.legs = []
-        self.current_pitch_z = 0.0
-        self.current_pitch_x = 0.0
-        self.current_yaw_x = 0.0
-        self.current_yaw_y = 0.0
-        self.current_roll_y = 0.0
-        self.current_roll_z = 0.0
+        self.current_position = [0.0, 0.0, 0.0]
+        self.current_rotation = [0.0, 0.0, 0.0]
         self.sc = ServoControl()
 
         for i in range(4):
@@ -45,42 +41,31 @@ class RobotDog:
                     print(f"Warning: Invalid target position for leg {i}: {targets[i]}")
             except Exception as e:
                 print(f"Error moving leg {i}: {e}")
-
-
-    def set_orientation(self):
-        new_positions = []
-        for i, leg in enumerate(self.legs):
-            x,y,z = LEGS_INTIAL_POSITIONS[i]
-            match leg.id:
-                case 0:
-                    new_x = x + self.current_pitch_x - self.current_yaw_x
-                    new_y = y + self.current_roll_y + self.current_yaw_y
-                    new_z = z - self.current_roll_z - self.current_pitch_z
-                case 1:
-                    new_x = x + self.current_pitch_x + self.current_yaw_x
-                    new_y = y - self.current_roll_y + self.current_yaw_y
-                    new_z = z + self.current_roll_z - self.current_pitch_z
-                case 2:
-                    new_x = x + self.current_pitch_x - self.current_yaw_x
-                    new_y = y + self.current_roll_y - self.current_yaw_y
-                    new_z = z - self.current_roll_z + self.current_pitch_z
-                case 3:
-                    new_x = x + self.current_pitch_x + self.current_yaw_x
-                    new_y = y - self.current_roll_y - self.current_yaw_y
-                    new_z = z + self.current_roll_z + self.current_pitch_z
-
-            new_positions.append([new_x,new_y,new_z])
-        self.move_legs(new_positions)
                 
 
-    def roll(self, alpha: float):
+    def roll(self, positions:List[List[float]], alpha: float, z:float):
         try:
-            delta_Y = 0.5 * self.body_width *( 1.0 - math.cos(alpha))
-            delta_Z = math.sin(alpha) * 0.5 * self.body_width
-            
-            self.current_roll_y = delta_Y
-            self.current_roll_z = delta_Z
-            print(f"delta_Y:{delta_Y}, delta_Z: {delta_Z}")
+            new_width = self.body_width * math.cos(alpha)
+            height_diff = math.sqrt(self.body_width**2 - new_width**2)
+            y_offset = self.body_width - new_width
+            new_positions = []
+            for i, position in enumerate(positions):
+                x, y, z = position
+                match i:
+                    case 0:
+                        height = z - height_diff
+                    case 1:
+                        y_offset = z + height_diff
+                    case 2:
+                        y_offset = z - height_diff
+                    case 3:
+                        y_offset *= z + height_diff
+    
+                new_Z = height * math.cos(alpha)
+                from_foot_to_shoulder = math.sqrt(y_offset**2 + height**2)
+                new_Y = math.sqrt(from_foot_to_shoulder**2 - new_Z**2)
+                new_positions.append([x, new_Y, new_Z])
+            self.move_legs(new_positions)
         except Exception as e:
             print(f"Error in roll movement: {e}")
 
